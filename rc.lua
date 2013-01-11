e7b2944c32574b24f7ce9c029c24bb1c91e1bbcc
Vincent Hobeïka <vincent.hobeika@gmail.com>
2013-01-11 08:17:16 +0100

    Added a loadavg widget at the top right of the status bar

--- rc.lua
+++ rc.lua
@@ -104,6 +104,21 @@ mylauncher = awful.widget.launcher({ image = image("/usr/share/icons/gnome/16x16
 -- Create a textclock widget
 mytextclock = awful.widget.textclock({ align = "right" })
 
+-- Execute a command and return its output. We may not only execute
+-- commands with a one line output.
+function execute_command(command)
+   local fh = io.popen(command)
+   local str = ""
+   for i in fh:lines() do
+      str = str .. i
+   end
+   io.close(fh)
+   return str
+end
+
+mytextloadavg = widget({ type = "textbox", name = "mytextloadavg" })
+mytextloadavg.text = " " .. execute_command("cat /proc/loadavg | cut -c -9") .. " "
+
 -- Create a systray
 mysystray = widget({ type = "systray" })
 
@@ -183,6 +198,7 @@ for s = 1, screen.count() do
         },
         mylayoutbox[s],
         mytextclock,
+        mytextloadavg,
         s == 1 and mysystray or nil,
         mytasklist[s],
         layout = awful.widget.layout.horizontal.rightleft
