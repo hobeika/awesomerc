637476296d1fc9863817787848edd7abd91cd181
Vincent Hobeïka <vincent.hobeika@gmail.com>
2013-01-11 17:00:48 +0100

    Trying to create a small module and an auto refreshing widget

--- rc.lua
+++ rc.lua
@@ -104,20 +104,12 @@ mylauncher = awful.widget.launcher({ image = image("/usr/share/icons/gnome/16x16
 -- Create a textclock widget
 mytextclock = awful.widget.textclock({ align = "right" })
 
--- Execute a command and return its output. We may not only execute
--- commands with a one line output.
-function execute_command(command)
-   local fh = io.popen(command)
-   local str = ""
-   for i in fh:lines() do
-      str = str .. i
-   end
-   io.close(fh)
-   return str
-end
+-- Loading our custom module
+require("hobeika.widget.textloadavg")
 
-mytextloadavg = widget({ type = "textbox", name = "mytextloadavg" })
-mytextloadavg.text = " " .. execute_command("cat /proc/loadavg | cut -c -9") .. " "
+mytextloadavg = hobeika.widget.textloadavg({ align = "right" })
+-- mytextloadavg = widget({ type = "textbox", name = "mytextloadavg" })
+-- mytextloadavg.text = " " .. execute_command("cat /proc/loadavg | cut -c -9") .. " "
 
 -- Create a systray
 mysystray = widget({ type = "systray" })
