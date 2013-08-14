12caed35fec63b7495c8fa9f3bac53b07712ac2b
Vincent Hobeïka <vincent.hobeika@gmail.com>
2013-08-14 08:53:27 +0200

    Changed bindings to match the bépo layout and avoid conflicts with mnemonic shortcuts

--- rc.lua
+++ rc.lua
@@ -213,12 +213,12 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
 
-    awful.key({ modkey,           }, "j",
+    awful.key({ modkey,           }, "l",
         function ()
             awful.client.focus.byidx( 1)
             if client.focus then client.focus:raise() end
         end),
-    awful.key({ modkey,           }, "k",
+    awful.key({ modkey,           }, "d",
         function ()
             awful.client.focus.byidx(-1)
             if client.focus then client.focus:raise() end
@@ -226,10 +226,10 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "w", function () mymainmenu:show({keygrabber=true}) end),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
-    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
+    awful.key({ modkey, "Shift"   }, "l", function () awful.client.swap.byidx(  1)    end),
+    awful.key({ modkey, "Shift"   }, "d", function () awful.client.swap.byidx( -1)    end),
+    awful.key({ modkey, "Control" }, "l", function () awful.screen.focus_relative( 1) end),
+    awful.key({ modkey, "Control" }, "d", function () awful.screen.focus_relative(-1) end),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
     awful.key({ modkey,           }, "Tab",
         function ()
@@ -246,17 +246,17 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
+    awful.key({ modkey,           }, "j",     function () awful.tag.incmwfact( 0.05)    end),
+    awful.key({ modkey,           }, "v",     function () awful.tag.incmwfact(-0.05)    end),
+    awful.key({ modkey, "Shift"   }, "v",     function () awful.tag.incnmaster( 1)      end),
+    awful.key({ modkey, "Shift"   }, "j",     function () awful.tag.incnmaster(-1)      end),
+    awful.key({ modkey, "Control" }, "v",     function () awful.tag.incncol( 1)         end),
+    awful.key({ modkey, "Control" }, "j",     function () awful.tag.incncol(-1)         end),
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.util.spawn("setxkbmap fr bepo") end),
+--    awful.key({ modkey, "Shift"   }, "k", function () awful.util.spawn("setxkbmap fr bepo") end),
 
     -- Prompt
     awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
