2f76062d3e56b7c54fb7f56c7f61b06c9bd54dfc
Vincent Hobeïka <vincent.hobeika@gmail.com>
2013-09-18 08:35:31 +0200

    tweaking colors

--- .config/awesome/themes/hal/theme.lua
+++ .config/awesome/themes/hal/theme.lua
@@ -6,19 +6,20 @@ theme = {}
 
 theme.font          = "sans 8"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
-theme.bg_urgent     = "#ff0000"
+theme.bg_normal     = "#151724"
+theme.bg_focus      = "#181828"
+theme.bg_urgent     = "#aa0000"
 theme.bg_minimize   = "#444444"
 
-theme.fg_normal     = "#aaaaaa"
+theme.fg_normal     = "#777777"
 theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
 theme.border_width  = "1"
 theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
+-- theme.border_focus  = "#535d6c"
+theme.border_focus  = "#cccccc"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
