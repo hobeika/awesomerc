769f05241d9b678e42be98455bcaa8d6dd432fc3
Vincent Hobeïka <vincent.hobeika@gmail.com>
2013-09-18 09:51:37 +0200

    Using environment variables in order to get rid of hard path.

--- .config/awesome/themes/hal/theme.lua
+++ .config/awesome/themes/hal/theme.lua
@@ -76,7 +76,9 @@ theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/defau
 
 -- You can use your own command to set your wallpaper
 -- theme.wallpaper_cmd = { "awsetbg /usr/share/awesome/themes/default/background.png" }
-theme.wallpaper_cmd = { "awsetbg -a -r /home/vincent/.config/awesome/wallpapers/" }
+local os = os
+wallpapers_path = os.getenv("WALLPAPERS")
+theme.wallpaper_cmd = { "awsetbg -a -r " .. wallpapers_path }
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
