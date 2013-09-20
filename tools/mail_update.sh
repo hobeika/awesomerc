f66bd82c18e4980b8b2a57f3cc740650bf434e7b
Vincent Hobeïka <vincent.hobeika@gmail.com>
2013-09-20 17:12:39 +0200

    Added some filters.

--- tools/mail_update.sh
+++ tools/mail_update.sh
@@ -6,5 +6,17 @@ OFFLINEIMAP_CONF_FILE=${HOME}/orgd/config/credentials/offlineimap.conf
 if [ -e "${OFFLINEIMAP_CONF_FILE}" ]; then
     # Get new mails.
     offlineimap -c "${OFFLINEIMAP_CONF_FILE}"
+
+    #FIXME Hook me up (see offlineimap documentation)
+
     notmuch new
+    # immediately archive all messages from "me"
+    notmuch tag -new -- tag:new and svnmailer Author: vhobeika
+    # Trac mails
+    notmuch tag +Trac -inbox -- tag:new and from:trac-cosmo@cosmo-platform.org
+    # svn mails
+    notmuch tag +svn -inbox -- tag:new from:svnmailer@cosmo-platform.org and not "/csmSDK/trunk/csmCore"
+    notmuch tag +svn -inbox -- tag:new from:svnmailer@cosmo-platform.org and not "in /csmSDK/trunk: csmCore/"
+    # calendar invitations and replies
+    notmuch tag +calendar -inbox -- tag:new "Invitation from Google Calendar"
 fi
