42fe16da6c1dd3907bd6b2f4b63291f00d6f689f
Vincent Hobeïka <vincent.hobeika@gmail.com>
2013-10-02 09:29:24 +0200

    Getting rid of the PATH helpers in case this is launched in a crontab.

--- tools/mail_update.sh
+++ tools/mail_update.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env dash
 
 OFFLINEIMAP_CONF_FILE=${HOME}/orgd/config/credentials/offlineimap.conf
+OFFLINEIMAP_BIN=/usr/bin/offlineimap
+NOTMUCH_BIN=/usr/bin/notmuch
 
 # If there is access to the offlineimap config file
 if [ -e "${OFFLINEIMAP_CONF_FILE}" ]; then
     # Get new mails.
-    offlineimap -c "${OFFLINEIMAP_CONF_FILE}"
+    ${OFFLINEIMAP_BIN} -c "${OFFLINEIMAP_CONF_FILE}"
 
     #FIXME Hook me up (see offlineimap documentation)
 
-    notmuch new
+    ${NOTMUCH_BIN} new
     # immediately archive all messages from "me"
-    notmuch tag -new -- tag:new and svnmailer Author: vhobeika
-    # Trac mails
-    notmuch tag +trac -inbox -- tag:new and from:trac-cosmo@cosmo-platform.org
+    ${NOTMUCH_BIN} tag -new -- tag:new and svnmailer Author: vhobeika
+    # trac mails
+    ${NOTMUCH_BIN} tag +trac -- tag:new and from:trac-cosmo@cosmo-platform.org
     # svn mails
-    notmuch tag +svn -inbox -- tag:new and from:svnmailer@cosmo-platform.org and not "/csmSDK/trunk/csmCore"
-    notmuch tag +svn -inbox -- tag:new and from:svnmailer@cosmo-platform.org and not "in /csmSDK/trunk: csmCore/"
+    ${NOTMUCH_BIN} tag +svn -- tag:new and from:svnmailer@cosmo-platform.org and not "/csmSDK/trunk/csmCore"
+    ${NOTMUCH_BIN} tag +svn -- tag:new and from:svnmailer@cosmo-platform.org and not "in /csmSDK/trunk: csmCore/"
     # calendar invitations and replies
-    notmuch tag +calendar -inbox -- tag:new and "Invitation from Google Calendar"
+    ${NOTMUCH_BIN} tag +calendar -inbox -- tag:new and "Invitation from Google Calendar"
+    # finally, retag all "new" messages "inbox" and "unread"
+    ${NOTMUCH_BIN} tag +inbox +unread -new -- tag:new
 fi
