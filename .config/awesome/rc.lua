ed82fe7c747fc3343995c0059b50e64287fae2d9
Vincent Hobeïka <vincent.hobeika@gmail.com>
2013-10-07 16:31:19 +0200

    assword window is now a floating window.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -374,6 +374,9 @@ awful.rules.rules = {
     { rule = { name = "Downloads" },
       properties = { floating = true,
                      } },
+    { rule = { name = "assword" },
+      properties = { floating = true,
+                     } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
