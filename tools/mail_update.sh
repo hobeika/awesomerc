89aba2ed22337bb0d1e01d3dc949a1715fa17907
Vincent Hobeïka <vincent.hobeika@gmail.com>
2013-10-18 15:49:44 +0200

    Fixing svn tag. They should now be flagged as read.

--- tools/mail_update.sh
+++ tools/mail_update.sh
@@ -17,10 +17,11 @@ if [ -e "${OFFLINEIMAP_CONF_FILE}" ]; then
     # trac mails
     ${NOTMUCH_BIN} tag +trac -- tag:new and from:trac-cosmo@cosmo-platform.org
     # svn mails
-    ${NOTMUCH_BIN} tag +svn -unread -- tag:new and from:svnmailer@cosmo-platform.org and not "/csmSDK/trunk/csmCore"
-    ${NOTMUCH_BIN} tag +svn -unread -- tag:new and from:svnmailer@cosmo-platform.org and not "in /csmSDK/trunk: csmCore/"
+    ${NOTMUCH_BIN} tag +svn -- tag:new and from:svnmailer@cosmo-platform.org and not "/csmSDK/trunk/csmCore"
+    ${NOTMUCH_BIN} tag +svn -- tag:new and from:svnmailer@cosmo-platform.org and not "in /csmSDK/trunk: csmCore/"
     # calendar invitations and replies
     ${NOTMUCH_BIN} tag +calendar -inbox -- tag:new and "Invitation from Google Calendar"
     # finally, retag all "new" messages "inbox" and "unread"
     ${NOTMUCH_BIN} tag +inbox +unread -new -- tag:new
+    ${NOTMUCH_BIN} tag -unread -- tag:svn
 fi
