f189a39aec822cd83b74ff3b22d42ee768dfa5e4
Vincent Hobeïka <vincent.hobeika@gmail.com>
2014-05-05 14:11:18 +0200

    Update awetest with bépo powered. Thanks Konubinix :)

--- .config/awesome/awetest.sh
+++ .config/awesome/awetest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-Xephyr -ac -br -noreset -screen 800x600 :2.0 &
+Xephyr -ac -br -retro -noreset -screen 1024x768 -keybd ephyr,xkbrules=evdev,xkbmodel=evdev,xkblayout=fr,xkbvariant=bepo :2.0 &
 sleep 1
 DISPLAY=:2.0 awesome -c /home/vincent/orgd/projects/cfg/.config/awesome/rc.lua
